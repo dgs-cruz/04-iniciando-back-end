91a770ef683f6415cecf63d9306f129e6b3dd07c
Douglas Cruz <douglas14dgs@hotmail.com>
2021-01-11 17:37:23 -0300

    Adicionada validação dos dados nas rotas com Celebrate

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ProfileController from '../controllers/ProfileController';
 
@@ -9,7 +10,25 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/', profileController.update);
 profileRouter.get('/', profileController.show);
+profileRouter.put(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            old_password: Joi.string(),
+            password: Joi.when('old_password', {
+                is: Joi.exist(),
+                then: Joi.required(),
+            }),
+            password_confirmation: Joi.when('password', {
+                is: Joi.exist(),
+                then: Joi.valid(Joi.ref('password')).required(),
+            }),
+        },
+    }),
+    profileController.update,
+);
 
 export default profileRouter;
